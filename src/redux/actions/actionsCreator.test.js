607925b5c440a0d5ecc334c1b9d814d3b48992b9
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-14 22:54:49 +0100

    tested the action

--- src/redux/actions/actionsCreator.test.js
+++ src/redux/actions/actionsCreator.test.js
@@ -1,4 +1,4 @@
-import { loadTasksAction } from "./actionsCreator";
+import { addTaskAction, loadTasksAction } from "./actionsCreator";
 import actionsTypes from "./actionsTypes";
 
 describe("Given a loadTasksAction function", () => {
@@ -12,6 +12,22 @@ describe("Given a loadTasksAction function", () => {
       };
 
       const action = loadTasksAction(task);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+describe("Given a addTask function", () => {
+  describe("When it is called with a task", () => {
+    test("then it should return an object with the action and the task", () => {
+      const task = { name: "test" };
+
+      const expectedAction = {
+        type: actionsTypes.addTask,
+        task: task,
+      };
+
+      const action = addTaskAction(task);
 
       expect(action).toEqual(expectedAction);
     });
