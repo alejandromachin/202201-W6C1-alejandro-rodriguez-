4e24d67c465f39febd5df5c14a17b81cca5e00ff
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-15 19:49:43 +0100

    added styles to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 import ToDoForm from "./components/ToDoForm/ToDoForm";
 import ToDoList from "./components/ToDoList/ToDoList";
 
 import { loadTasksThunk } from "./redux/thunks/tasksThunks";
-
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 200px;
+`;
 const App = () => {
   const tasksList = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
@@ -16,8 +23,10 @@ const App = () => {
 
   return (
     <>
-      <ToDoForm />
-      <ToDoList tasks={tasksList} />
+      <PageContainer>
+        <ToDoForm />
+        <ToDoList tasks={tasksList} />
+      </PageContainer>
     </>
   );
 };
